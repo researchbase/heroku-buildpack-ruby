9b0324ad574431f6da875c6a9ba976784e003ee1
Adam Meehan <adam.meehan@gmail.com>
2017-12-06 18:37:02 +1100

    Add an output when fetching file

--- lib/language_pack/installers/heroku_ruby_installer.rb
+++ lib/language_pack/installers/heroku_ruby_installer.rb
@@ -19,6 +19,7 @@ class LanguagePack::Installers::HerokuRubyInstaller
         ruby_vm = "ruby"
         file.sub!(ruby_vm, "#{ruby_vm}-build")
       end
+      puts "Fetching #{file}"
       @fetcher.fetch_untar(file)
     end
   end
