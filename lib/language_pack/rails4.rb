982fc54541ed2699244e17c6b373cad6c28b0f2d
Adam Meehan <adam.meehan@gmail.com>
2018-05-03 21:10:23 +1000

    remove plugins in ruby pack

--- lib/language_pack/rails4.rb
+++ lib/language_pack/rails4.rb
@@ -45,17 +45,7 @@ class LanguagePack::Rails4 < LanguagePack::Rails3
   private
 
   def install_plugins
-    instrument "rails4.install_plugins" do
-      return false if bundler.has_gem?('rails_12factor')
-      plugins = ["rails_serve_static_assets", "rails_stdout_logging"].reject { |plugin| bundler.has_gem?(plugin) }
-      return false if plugins.empty?
-
-    warn <<-WARNING
-Include 'rails_12factor' gem to enable all platform features
-See https://devcenter.heroku.com/articles/rails-integration-gems for more information.
-WARNING
-    # do not install plugins, do not call super
-    end
+    # no plugins for us
   end
 
   def public_assets_folder
